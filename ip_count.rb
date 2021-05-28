5c4b56e11f1de03d61b0b8329549e3215e157a31
Matt Rush <matthew.rush@trustedsite.com>
2021-05-28 10:55:12 -0600

    Added support for internal and external networks, providing a summary for both network types.

--- ip_count.rb
+++ ip_count.rb
@@ -16,7 +16,7 @@
 require 'netaddr'
 
 ################
-# settings
+# data
 ################
 
 same_line  = "\x1B[1A" # move cursor up one line
@@ -44,8 +44,25 @@ def format_heading(title)
   STDERR.puts "[#{title.upcase.chomp}]"
 end
 
+def is_private?(ip)
+  unless ip =~ /^(\d{1,3}).(\d{1,3}).(\d{1,3}).(\d{1,3})$/
+    raise "#{ip} is not a valid ip address"
+  end
+
+  octets = [$1,$2,$3,$4].map(&:to_i)
+  raise "#{ip} is not a bad ip address" unless octets.all? { |o| o < 256 }
+
+  (octets[0] == 10) ||
+  (
+    (octets[0] == 172) && (octets[1] >= 16) && (octets[1] <= 31)
+  ) ||
+  (
+    (octets[0] == 192) && (octets[1] == 168)
+  )
+end
+
 ################
-# arguments
+# parameters
 ################
 
 file_name = ARGV[0]
@@ -59,31 +76,78 @@ end
 # rc
 ################
 
-format_heading('settings')
+# init
+
+format_heading('init')
 format_message("#{file_name}",'input file')
+
+# read in txt file
+
+lines = File.readlines(file_name)
+total_lines = lines.count
+format_message(total_lines,"network count")
+
+# find internal vs. external hosts
 
 STDERR.puts('')
 format_heading('progress')
 
-lines = File.readlines(file_name)
-total_lines = lines.count
-
-total_hosts = 0
-current_line = 0
-
+internal_hosts = []
+external_hosts = []
 lines.each do |line|
-  current_line += 1
+  d = {}
 
-  STDERR.print(same_line) if current_line > 1
-  format_notice("#{current_line}/#{total_lines}",'completed')
+  network = NetAddr::IPv4Net.parse(line)	# convert string to netaddr object
+  ip_net_name = network.network.to_s		# get network name as string from netaddr object
 
-  network = NetAddr::IPv4Net.parse(line)
+  if is_private?(ip_net_name)
+    network_type = "private"
+  else
+    network_type = "public"
+  end
+ 
+  count_tmp = network.len - 2 
+ 
+  format_message("#{network.to_s}][#{count_tmp}","#{network_type}")
 
-  total_hosts += network.len - 2
-  format_notice("subtotal: #{network.len - 2} / total: #{total_hosts}","network: #{line.chomp}")
+
+
+  # build d hash.
+
+  d[:network] = network.to_s
+  d[:size] = network.len - 2
+
+  internal_hosts.push(d) if network_type == "private"
+  external_hosts.push(d) if network_type == "public"
 end
 
-STDERR.puts('')
-format_heading('total assignable addresses')
+# summarize
 
-STDOUT.puts total_hosts
+STDERR.puts('')
+format_heading('summary')
+
+#   internal
+internal_networks = internal_hosts.count
+
+total_internal_hosts = 0
+internal_hosts.each do |host|
+  total_internal_hosts += host[:size]
+end
+
+format_message("networks: #{internal_networks}][addresses: #{total_internal_hosts}",'internal')
+
+#   external
+external_networks = external_hosts.count
+
+total_external_hosts = 0
+external_hosts.each do |host|
+  total_external_hosts += host[:size]
+end
+
+format_message("networks: #{external_networks}][addresses: #{total_external_hosts}",'external')
+
+#   combined
+all_networks = internal_networks + external_networks
+all_hosts = total_internal_hosts + total_external_hosts
+
+format_message("networks: #{all_networks}][addresses: #{all_hosts}",'total')
